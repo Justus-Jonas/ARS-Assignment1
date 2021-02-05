685cb69d49d45b1da271e7547e44b9cf790d7e20
dhruv2601 <dhruvrathi15@gmail.com>
2021-02-05 12:36:11 +0100

    Added dynamic Inertia

--- PSO_Algo.py
+++ PSO_Algo.py
@@ -1,12 +1,14 @@
 class PSO():
   def __init__(self, benchmark, ideal_target, error_threshold, num_particles,
                 a_const = 0.9, b_const= 2, c_const = 2):
-    self.error_threshold = error_threshold
     self.benchmark = benchmark
     self.a_const = a_const
     self.b_const = b_const
     self.c_const = c_const
+    self.ideal_target = ideal_target
+    self.error_threshold = error_threshold
     self.num_particles = num_particles
+    self.error_recorder = []
     self.gbest_val = float('inf')
     self._create_particles()
     self._init_global_best_pos()
@@ -20,24 +22,27 @@ class PSO():
     # Setting the initial params of the particles
     for ind_particle in range(self.num_particles):
       self.particle_curr_pos[ind_particle][0] = (-1)**(bool(random.getrandbits(1))) * \
-                                                random.random()*50   # random start point x
+                                                random.random()   # random start point x
       self.particle_curr_pos[ind_particle][1] = (-1)**(bool(random.getrandbits(1))) * \
-                                                random.random()*50   # random start point y
+                                                random.random()   # random start point y
       self.particle_best_pos[ind_particle][0] = self.particle_curr_pos[ind_particle][0]  # init best pos x = start pos x
       self.particle_best_pos[ind_particle][1] = self.particle_curr_pos[ind_particle][1]  # init best pos y = start pos y
 
   def _init_global_best_pos(self):
     self.gbest_pos = np.zeros(2)    # universally one in global topology, therefore just a 2d array required
-    self.gbest_pos[0] = random.random()*50
-    self.gbest_pos[1] = random.random()*50
+    # self.gbest_pos[0] = random.random()
+    # self.gbest_pos[1] = random.random()
+    self.gbest_pos[0] = -1
+    self.gbest_pos[1] = -1
+
 
   def pbest_setter(self):
     for particle in range(self.num_particles):
       benchmark_reward_candidate = benchmark(self.particle_curr_pos[particle][0], \
                                              self.particle_curr_pos[particle][1]) #TODO: restructure how rosenbrock and other is defined! 
-      if(self.particle_best_val[particle] > benchmark_reward_val):
+      if(self.particle_best_val[particle] > benchmark_reward_candidate):
         #update best value and corresponding position
-        self.particle_best_val[particle] = benchmark_reward_val
+        self.particle_best_val[particle] = benchmark_reward_candidate
         self.particle_best_pos[particle] = self.particle_curr_pos[particle]
 
   def gbest_setter(self):
@@ -56,8 +61,8 @@ class PSO():
 
       # TODO: discuss the significance of del t in the position equation.
       new_position = self.particle_curr_pos[particle] + new_velocity  
-      particle_curr_velo[particle] = new_velocity # update particle velocity
-      particle_curr_pos[particle] = new_position  # update particle position
+      self.particle_curr_velo[particle] = new_velocity # update particle velocity
+      self.particle_curr_pos[particle] = new_position  # update particle position
 
   def ps_optimizer(self, num_iterations):
     iter = 0
@@ -65,11 +70,20 @@ class PSO():
       self.pbest_setter()
       self.gbest_setter()
 
+      self.error_recorder.append(abs(self.ideal_target - self.gbest_val))
+
       if (self.error_threshold >= abs(self.ideal_target - self.gbest_val)):
+        '''
+        If the defined error threshold is acheived, return it.
+        '''
+        return self.error_recorder
         break;
       
       '''
-      Parameter Optimization: SOTA for PSO paramtere optimzation states that
+      Parameter Optimization: 
+      Here, we compare the results of a constant inertia 
+      and time-varying inertia.
+      SOTA for PSO paramtere optimzation states that
       inertia(a) should be gradually decreased from 0.9 to 0.4 over the defined
       iterations. The code below will start with a=0.9 and linearly reduce the 
       inertia parameter to 0.4
@@ -77,3 +91,4 @@ class PSO():
       self.a_const = 0.9 - (float(0.5)*(float(iter)/num_iterations))
       self.start_particle_motion()
       iter += 1
+    return self.error_recorder
